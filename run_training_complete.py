d1afe1bf2d05f0db151de9798ba25794597e2e7a
Riccardo Merlo <merloriki97@live.it>
2022-06-27 17:02:06 +0200

    fix

--- run_training_complete.py
+++ run_training_complete.py
@@ -32,36 +32,29 @@ Read CORPUS
 """
 text = read_corpus('./corpus/nyt_articles_v2.txt')
 
-"""
-Create DATASET
-"""
-data, unigram_counts, vocab, inv_vocab = create_skipgram(
-    text, WINDOW_SIZE, WEATLIST.copy(), MIN_FREQ, SAMPLING_RATE, EPOCHS, BATCH_SIZE)
-
-with open('data.pkl', 'wb') as f:
-  pickle.dump(data, f)
-
-with open('unigram_counts.pkl', 'wb') as f:
-  pickle.dump(unigram_counts, f)
-
-with open('vocab.pkl', 'wb') as f:
-  pickle.dump(vocab, f)
-
-with open('inv_vocab.pkl', 'wb') as f:
-  pickle.dump(inv_vocab, f)
 
 
 """
 Create MODEL
 """
-word2vec = Word2VecModel(unigram_counts,
-                         hidden_size=300,
+word2vec = Word2VecModel(hidden_size=300,
                          batch_size=BATCH_SIZE,
                          negatives=NEGATIVES,
                          power=0.75,
                          alpha=LEARNING_RATE)
 
 """
+Build DATASET
+"""
+word2vec.build_dataset(text, WEATLIST.copy(), MIN_FREQ, SAMPLING_RATE)
+
+"""
+Build WEIGHTS
+"""
+word2vec.build_weights()
+
+
+"""
 Train MODEL
 """
-word2vec.train(data, save=True)
+word2vec.train(EPOCHS, WINDOW_SIZE, save=True)
